4700065472c09cbe5fe3e50cebde96b351e020d2
andre junior <andrebonifaacio@gmail.com>
2022-03-13 19:45:13 -0300

    implementando análise de frequência

--- src/cifra/CifraVigenere.java
+++ src/cifra/CifraVigenere.java
@@ -1,4 +1,6 @@
 package src.cifra;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CifraVigenere {
@@ -10,7 +12,7 @@ public class CifraVigenere {
     public static void main(String[] args) {
 
         in = new Scanner(System.in);
-        System.out.println("1.Cifrar:\n2.Decifrar\nSelecione(1,2):");
+        System.out.println("1.Cifrar:\n2.Decifrar\n3.Analise de frequencia\nSelecione(1,2,3):");
         int escolha = in.nextInt();
         in.nextLine();
 
@@ -18,17 +20,19 @@ public class CifraVigenere {
             System.out.println("-----Cifrador-----");
             menuMensagemChave();
             cifraMensagem(mensagem, chave2);
-        } else if (escolha == 2) {
+        }
+        if (escolha == 2) {
             System.out.println("-----Decifrador-----");
             menuMensagemChave();
             decifraMensagem(mensagem, chave2);
-        } else {
-            System.out.println("Escolha inexistente, por favor escolha entre as opções 1 ou 2.");
+        } else if (escolha == 3) {
+            menuMensagemChave2();
         }
 
     }
 
-    private static void cifraMensagem(String mensagem, String chave2 ) {
+
+    private static void cifraMensagem(String mensagem, String chave2) {
         int[][] tabela = tabelaVigenere();
         String texto = "";
 
@@ -45,15 +49,15 @@ public class CifraVigenere {
 
     }
 
-    private static void decifraMensagem(String mensagem, String chave2){
+    private static void decifraMensagem(String mensagem, String chave2) {
         int[][] tabela = tabelaVigenere();
         String textoDecifrado = "";
 
-        for(int i = 0; i<mensagem.length(); i++){
-            if(mensagem.charAt(i) == (char) 32 && chave2.charAt(i) == (char) 32){
+        for (int i = 0; i < mensagem.length(); i++) {
+            if (mensagem.charAt(i) == (char) 32 && chave2.charAt(i) == (char) 32) {
                 textoDecifrado += " ";
             } else {
-                textoDecifrado += (char) (65 + contadorItr((int)chave2.charAt(i), (int)mensagem.charAt(i)));
+                textoDecifrado += (char) (65 + contadorItr((int) chave2.charAt(i), (int) mensagem.charAt(i)));
             }
         }
 
@@ -61,50 +65,43 @@ public class CifraVigenere {
 
     }
 
-    private static int contadorItr(int chave, int mensagem){
+    private static int contadorItr(int chave, int mensagem) {
         int contador = 0;
         String resultado = "";
-        for(int i = 0; i < 26; i++){
-            if(chave + i > 90){
-                resultado += (char) (chave + (i-26));
-            }else{
+        for (int i = 0; i < 26; i++) {
+            if (chave + i > 90) {
+                resultado += (char) (chave + (i - 26));
+            } else {
                 resultado += (char) (chave + i);
             }
         }
 
-        for(int i = 0; i < resultado.length(); i++){
-            if(resultado.charAt(i) == mensagem)  {
+        for (int i = 0; i < resultado.length(); i++) {
+            if (resultado.charAt(i) == mensagem) {
                 break;
-            }else{  
-        
-            contador++;
+            } else {
+
+                contador++;
             }
         }
         return contador;
     }
 
-
+    //matriz para a criação da tabela de Vigenere
     private static int[][] tabelaVigenere() {
         int[][] array = new int[26][26];
         for (int i = 0; i < 26; i++) {
             for (int j = 0; j < 26; j++) {
-                int temp;
+                int aux;
                 if ((i + 65) + j > 90) {
-                    temp = ((i + 65) + j) - 26;
-                    array[i][j] = temp;
+                    aux = ((i + 65) + j) - 26;
+                    array[i][j] = aux;
                 } else {
-                    temp = (i + 65) + j;
-                    array[i][j] = temp;
+                    aux = (i + 65) + j;
+                    array[i][j] = aux;
                 }
             }
         }
-
-        // for(int i = 0; i<26;i++){
-        // for(int j=0;j<26;j++){
-        // System.out.print((char)array[i][j] + "");
-        // }
-        // System.out.println();
-        // }
 
         return array;
     }
@@ -138,8 +135,130 @@ public class CifraVigenere {
         mensagem = msg;
         chave2 = chaveMap;
 
-        // System.out.println("Mensagem: " + mensagem);
-        // System.out.println("Chave: "+ chave);
+    }
+
+    private static void menuMensagemChave2() {
+
+        String msg1 = "rvgllakieg tye tirtucatzoe. whvnvvei i" +
+                "winu mpsecf xronieg giid abfuk thv mfuty; wyenvvvr ik ij a drmg," +
+                "drzzqly eomemsei in dy jouc; wyenvvvr i wied mpsvlf znmollnkarzlp " +
+                "palszng seworv cfffzn narvhfusvs, rnd srzngznx up khv rerr ff emeiy" +
+                "flnvrac i deek; aed ejpvcirlcy wyeeevvr dy hppfs gvt jucy ae upgei" +
+                "haed ff mv, tyat zt ieqliies r skroeg dorrl grieczplv tf prvvvnt de" +
+                "wrod dvliseiatvlp stvpginx ieto khv stievt, aed detyouicrlcy keotkieg" +
+                "geoglv's hrtj ofw--tyen, z atcolnk it yixh tzmv to xek to jer as jofn" +
+                "aj i tan.  khzs ij mp susskitltv foi pzstfl rnd sacl.  wzty a" +
+                "pyicosfpyicrl wlolrzsh tako tyrfws yidsecf lpoe hzs snoid; i huzetcy" +
+                "kakv tf thv syip.  khvre zs eotyieg slrgrijieg ie tyis.  zf khep blt" +
+                "keen it, rldosk acl mvn zn tyezr dvgiee, jode tzmv or ftyer, thvrijh" +
+                "merp nvarcy khe jade fvecinxs kowrrus tye fcern nity mv.";
+        msg1 = msg1.toUpperCase();
+
+        System.out.println(
+                "Digite o tamanho estimado da chave:");
+        System.out.println(crackChave(msg1, in.nextInt()));
+
+    }
+
+    public static double[] LETTER_FREQUENCIES = new double[] {
+            8.167,
+            1.492,
+            2.782,
+            4.253,
+            12.702,
+            2.228,
+            2.015,
+            6.094,
+            6.966,
+            0.153,
+            0.772,
+            4.025,
+            2.406,
+            6.749,
+            7.507,
+            1.929,
+            0.095,
+            5.987,
+            6.327,
+            9.056,
+            2.758,
+            0.978,
+            2.360,
+            0.150,
+            1.974,
+            0.074
+    };
+
+    public static String numberToKey(long chaveAtual) {
+        if (chaveAtual < 0) {
+            return "";
+        } else {
+            return numberToKey((chaveAtual / 26) - 1) + (char) (65 + chaveAtual % 26);
+        }
+    }
+
+    public static String crackChave(String encString, int maxKeyLength) {
+        long chave = 0;
+        double desloc = Double.MAX_VALUE;
+        for (long chaveAtual = 1; chaveAtual < Math.pow(27, maxKeyLength); chaveAtual++) {
+            double offset = calculaFreqDif(codificaString(encString, numberToKey(chaveAtual), false));
+
+            if (offset < desloc) {
+                desloc = offset;
+                chave = chaveAtual;
+            }
+        }
+
+        return numberToKey(chave);
+    }
+
+    public static String codificaString(String s, String keyString, Boolean menu) {
+        StringBuilder textoCodificado = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int chave = ((int) keyString.charAt(i % keyString.length())) - 65;
+            if (menu) {
+                textoCodificado.append(codificador(s.charAt(i), chave));
+            } else {
+                textoCodificado.append(codificador(s.charAt(i), -chave));
+            }
+
+        }
+        return textoCodificado.toString();
+    }
+
+    public static double calculaFreqDif(String posDecString) {
+        char[] Array = posDecString.toCharArray();
+        Arrays.sort(Array);
+        String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        double atualFreq = 0;
+        String msgOrdenada = (Arrays.toString(Array));
+
+        for (int i = 0; i < alfabeto.length(); i++) {
+            String c = Character.toString(alfabeto.charAt(i));
+            int charAmount = msgOrdenada.length() - msgOrdenada.replace(c, "").length();
+            double desloc = (LETTER_FREQUENCIES[i] - charAmount / (double) posDecString.length());
+            desloc *= desloc;
+            atualFreq += desloc;
+        }
+
+        return atualFreq / 26D;
+    }
+
+    public static char codificador(char c, int chave) {
+
+        if ((int) c == 32) {
+            return (char) 32; 
+
+        } else if (((int) c + chave) > 90) {
+            return (char) (((int) c + chave) - 26);
+
+        } else if (((int) c + chave) < 65) {
+            return (char) ((int) c + 26 + chave);
+
+        } else {
+            return (char) ((int) c + chave);
+
+        }
     }
 
 }
